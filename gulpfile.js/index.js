480f1717530ab9d272101ba7bb3508590519a7ee
Pete Brousalis <brousapg@gmail.com>
2015-10-13 12:29:43 -0500

    setting up example

--- gulpfile.js/index.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var requireDir = require('require-dir');
-
-// Require all tasks in gulpfile.js/tasks
-requireDir('./tasks', { recurse: true })
-
-// Default task runs the dev server
-gulp.task('default', ['server']);
